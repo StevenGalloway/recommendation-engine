c22d2a1b19acbc6dd74f8f14499da67fe894df9f
Steven Galloway <stevenpaulgalloway@gmail.com>
2026-05-03 08:35:22 -0500

    initialize Next.js project config

---
+++ anime_recommend/next.config.ts
@@ -0,0 +1,23 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.myanimelist.net',
+        port: '',
+        pathname: '/images/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
